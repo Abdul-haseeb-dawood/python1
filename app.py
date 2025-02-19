d477499085152f540adf135df5c94ac2416f0c21
Abdul-haseeb-dawood <165469857+Abdul-haseeb-dawood@users.noreply.github.com>
2025-02-19 22:41:11 +0500

    app.py

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ from io import BytesIO
 # Set up our app
 
 st.set_page_config(page_title='Data sweeper',layout='wide') 
-st.title('Data sweeper')
+
+
+# custum css
+st.markdown(
+    """
+    <style>
+        .stApp{
+        background-color: black;
+        color: white;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+)
+
+st.title(" Data-sweeper sterling integrator by A.Haseeb")
 st.write("transform from your file bettween CSV & Excel formats with build in data cleaning & visualization")
+
+#file uploder
+
+uploded_files = st.file_uploader("Uplode your files (accepts csv or excel ):" ,type=["csv","xlsx"],
+accept_multiple_files=(True))
